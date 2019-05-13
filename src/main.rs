f593c21b0848c60f6b1c60cae8dbb065f2fb07f1
JanKaul <jankaul@mailbox.org>
2019-05-13 19:47:40 +0200

    added Constant in Exp

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ type Var = String;
 
 #[derive(Clone,Debug)]
 enum Exp {
+    Constant(Var),
     Var(Var),
     Lam(Var,Box<Exp>),
     App(Box<Exp>,Box<Exp>)
@@ -37,16 +38,20 @@ fn step(state: State) -> State {
         (Exp::Var(x),e1,k) => match e1.get(&x).unwrap() {
             // if its a closure, use its environment and evaluate contained lambda expression
             Value::Closure((t,e2)) => (t.clone(),e2.clone(),k),
-            // if its a constant, done
-            Value::Constant(_) => (Exp::Var(x),e1,Continuation::Done)
+            // if its a constant, put constant in control
+            Value::Constant(x2) => (Exp::Constant(x2.clone()),e1,k)
         },
         // case: Control == Application -> put Lambda term into control and put Argument into EvalArg continuation
         (Exp::App(t1,t2),e,k) => (*t1,e.clone(),Continuation::EvalArg(*t2,e,Box::new(k))),
         // case: Control == Lambda && Cont == EvalArg -> put lambda expression into Call continuation and evaluate expression from EvalArg continuation
         (Exp::Lam(x1,t1),e1,Continuation::EvalArg(t2,e2,k)) => (t2,e2,Continuation::Call(Exp::Lam(x1,t1),e1,k)),
-        // case: Control == Lambda && Cont == Call ->   set the argument of the Call continuation equal to the Closure of the Lambda expression and
-        //                                              its environment inside the environment of the Call continuation
+        // case: Control == Lambda && Cont == Call ->   set the argument of the Call continuation inside the environment of the Call continuation
+        //                                              equal to the Closure of the Lambda expression and its environment
         (Exp::Lam(x2,t2),e2,Continuation::Call(Exp::Lam(x1,t1),mut e1,k)) => {e1.insert(x1,Value::Closure((Exp::Lam(x2,t2),e2)));(*t1,e1,*k)},
+        // case: Control == Constant && continuation == Call -> set argument of Call continuation to the constant in the environment of the call continuation
+        (Exp::Constant(x2),_,Continuation::Call(Exp::Lam(x1,t1),mut e1,k)) => {e1.insert(x1,Value::Constant(x2));(*t1,e1,*k)},
+        // case: Control == Constant && continuation == else -> Done
+        (Exp::Constant(x2),e,k) => (Exp::Constant(x2),e,k),
         // else
         (c,e,_) => (c,e,Continuation::Failed)
     }
@@ -56,10 +61,15 @@ fn main() {
 
     let env : Environment = HashMap::new();
 
+    // example 1
     let e1 = Box::new(Exp::Lam(String::from("f"),Box::new(Exp::App(Box::new(Exp::Var(String::from("f"))),Box::new(Exp::Var(String::from("f")))))));
     let e2 = Box::new(Exp::Lam(String::from("y"),Box::new(Exp::Var(String::from("y")))));
 
-    let mut state = (Exp::App(e1,e2),env,Continuation::Done);
+    //example 2
+    let e3 = Box::new(Exp::Lam(String::from("f"),Box::new(Exp::App(Box::new(Exp::Var(String::from("f"))),Box::new(Exp::Constant(String::from("1")))))));
+    let e4 = Box::new(Exp::App(Box::new(Exp::Lam(String::from("x"),Box::new(Exp::Lam(String::from("y"),Box::new(Exp::Var(String::from("x"))))))),Box::new(Exp::Constant(String::from("2")))));
+
+    let mut state = (Exp::App(e3,e4),env,Continuation::Done);
 
     dbg!(&state);
     for _i in 1..12 {
